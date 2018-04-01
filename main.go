98265780ba55c0a91c8ffb30cafa9d5002daa7cd
Dominic Ginger <domginger@hotmail.co.uk>
2018-04-02 00:22:48 +0100

    Pretty print the board using unicode characters

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 )
 
+var xAxis = "ABCDEFGH"
+var yAxis = [...]int{1, 2, 3, 4, 5, 6, 7, 8}
+
 type Square struct {
 	coloured bool
-	x        int
+	x        byte
 	y        int
 }
 
@@ -14,25 +17,57 @@ type Board struct {
 	rows [8][8]Square
 }
 
-var xAxis = [...]int{67, 68, 69, 70, 71, 72, 73, 74}
-var yAxis = [...]int{1, 2, 3, 4, 5, 6, 7, 8}
+func (b *Board) print() {
+	fmt.Printf(" ")
+	for _, c := range xAxis {
+		fmt.Printf("  %c ", c)
+	}
+	fmt.Println()
 
-func main() {
-	board := Board{}
+	for i := range b.rows {
+		fmt.Printf("  ")
+		for _, s := range b.rows[i] {
+			if s.coloured {
+				fmt.Printf("■■■■")
+			} else {
+				fmt.Printf("□□□□")
+			}
+		}
+		fmt.Printf("\n%d ", b.rows[i][0].y)
+		for _, s := range b.rows[i] {
+			if s.coloured {
+				fmt.Printf("■♔ ■")
+			} else {
+				fmt.Printf("□□□□")
+			}
+		}
+		fmt.Printf("\n  ")
+		for _, s := range b.rows[i] {
+			if s.coloured {
+				fmt.Printf("■■■■")
+			} else {
+				fmt.Printf("□□□□")
+			}
+		}
 
+		fmt.Println()
+	}
+}
+
+func newBoard() (board Board) {
 	coloured := true
+
 	for i := range board.rows {
 		coloured = !coloured
 		for j := range board.rows[i] {
-			board.rows[i][j] = Square{coloured, xAxis[i], yAxis[j]}
-			if coloured {
-				fmt.Printf("#")
-			} else {
-				fmt.Printf(" ")
-			}
-				fmt.Printf(" %d %d", board.rows[i][j].x, board.rows[i][j].y)
+			board.rows[i][j] = Square{coloured, xAxis[j], 9 - yAxis[i]}
 			coloured = !coloured
 		}
-		fmt.Println()
 	}
+	return
+}
+
+func main() {
+	board := newBoard()
+	board.print()
 }

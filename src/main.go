cf83ed97d66bf8c002561e9c997944218ee8ce69
Dominic Ginger <domginger@hotmail.co.uk>
2018-04-04 10:32:43 +0100

    Add valid move check for pawns

--- src/main.go
+++ src/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	board := newBoard()
 	for i := 0; i < 8; i++ {
@@ -7,19 +11,20 @@ func main() {
 		board.addPiece(i, 6, newPawn(false))
 	}
 
-	type placement struct {
-		y int
-		coloured bool
-	}
-	for _, p := range []placement{placement{0, true}, placement{7, false}} {
-		board.addPiece(0, p.y, newRook(p.coloured))
-		board.addPiece(1, p.y, newKnight(p.coloured))
-		board.addPiece(2, p.y, newBishop(p.coloured))
-		board.addPiece(3, p.y, newQueen(p.coloured))
-		board.addPiece(4, p.y, newKing(p.coloured))
-		board.addPiece(5, p.y, newBishop(p.coloured))
-		board.addPiece(6, p.y, newKnight(p.coloured))
-		board.addPiece(7, p.y, newRook(p.coloured))
-	}
+	fmt.Println(board.rows[0][1].piece.validMove(move{0, 1}, move{0, 2}))
+	// type placement struct {
+	// 	y int
+	// 	coloured bool
+	// }
+	// for _, p := range []placement{placement{0, true}, placement{7, false}} {
+	// 	board.addPiece(0, p.y, newRook(p.coloured))
+	// 	board.addPiece(1, p.y, newKnight(p.coloured))
+	// 	board.addPiece(2, p.y, newBishop(p.coloured))
+	// 	board.addPiece(3, p.y, newQueen(p.coloured))
+	// 	board.addPiece(4, p.y, newKing(p.coloured))
+	// 	board.addPiece(5, p.y, newBishop(p.coloured))
+	// 	board.addPiece(6, p.y, newKnight(p.coloured))
+	// 	board.addPiece(7, p.y, newRook(p.coloured))
+	// }
 	board.print()
 }
--- src/pieces.go
+++ src/pieces.go
@@ -1,8 +1,25 @@
 package main
 
+type move struct {
+	x int
+	y int
+}
+
+type ChessPiece interface {
+	validMove(from, to move) bool
+	// validMoves(m move) []move
+}
+
 type Piece struct {
 	coloured bool
 	symbol string
+}
+
+func (p *Piece) validMove(from, to move) bool {
+	if p.coloured {
+		return to.y == from.y - 1 || to.y == from.y - 2
+	}
+	return to.y == from.y + 1 || to.y == from.y + 2
 }
 
 var symbols = [...]string{"♙", "♟", "♘", "♞", "♗", "♝", "♖", "♜", "♕", "♛", "♔", "♚"}
@@ -19,23 +36,23 @@ func newPawn(coloured bool) (p Piece) {
 	return Piece{coloured, getSymbol(0, coloured)}
 }
 
-func newKnight(coloured bool) (p Piece) {
-	return Piece{coloured, getSymbol(2, coloured)}
-}
+// func newKnight(coloured bool) (p Piece) {
+// 	return Piece{coloured, getSymbol(2, coloured)}
+// }
 
-func newBishop(coloured bool) (p Piece) {
-	return Piece{coloured, getSymbol(4, coloured)}
-}
+// func newBishop(coloured bool) (p Piece) {
+// 	return Piece{coloured, getSymbol(4, coloured)}
+// }
 
-func newRook(coloured bool) (p Piece) {
-	return Piece{coloured, getSymbol(6, coloured)}
-}
+// func newRook(coloured bool) (p Piece) {
+// 	return Piece{coloured, getSymbol(6, coloured)}
+// }
 
-func newQueen(coloured bool) (p Piece) {
-	return Piece{coloured, getSymbol(8, coloured)}
-}
+// func newQueen(coloured bool) (p Piece) {
+// 	return Piece{coloured, getSymbol(8, coloured)}
+// }
 
-func newKing(coloured bool) (p Piece) {
-	return Piece{coloured, getSymbol(10, coloured)}
-}
+// func newKing(coloured bool) (p Piece) {
+// 	return Piece{coloured, getSymbol(10, coloured)}
+// }
 
